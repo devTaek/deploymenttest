2d48b6b90a90a6be61df396df81a194e9a9e37e7
d-taegyun <d.taegyun@gmail.com>
2024-04-19 00:39:50 +0900

    빌드 scripts 추가

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 3000);
 
 app.use(express.static(path.join(__dirname, "frontend_with_react/build")));
 
